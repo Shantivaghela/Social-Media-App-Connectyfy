f4bf44d53cfb4079e771fa2ec0b576a7e541ed62
Shantivaghela <shantivaghela2019@gmail.com>
2025-01-31 16:20:39 +0530

    admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Home from './pages/home'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
     <Header/>
-   
-  )
+   <Home/>
+   </>
+     )
 }
 
 export default App
