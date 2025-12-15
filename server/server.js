bd769f2bfb73638867e4702cebb922c5c7d3b531
Shantivaghela <shantivaghela2019@gmail.com>
2025-12-16 02:28:51 +0530

    add file

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const connectDB = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 const storyrouter = require('./router/story-router');
 const messagerouter = require('./router/message-router');
-// const {server,app} = require('./utils/socketUsers')
+const {server,app} = require('./utils/socketUsers')
 const adminrouter = require('./router/admin-router')
 
 // app.use(express.json({ limit: "1000mb" }));
@@ -37,7 +37,7 @@ app.use("/api/admin/",adminrouter);
 
 
 app.use(errorMiddleware);
-module.exports = app;
+// module.exports = app;
 const PORT = process.env.PORT || 8080;
 
 connectDB().then(()=>{
