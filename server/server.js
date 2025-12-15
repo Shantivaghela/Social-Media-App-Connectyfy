2e52297e5f519777c6beee2f2bbf187b354a843e
Shantivaghela <shantivaghela2019@gmail.com>
2025-12-16 02:17:32 +0530

    add vercel.json file

--- server/server.js
+++ server/server.js
@@ -34,9 +34,7 @@ app.use("/api/post/",postDatarouter);
 app.use("/api/story/",storyrouter);
 app.use("/api/message/",messagerouter);
 app.use("/api/admin/",adminrouter);
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'Backend working! 🚀' });
-});
+
 
 app.use(errorMiddleware);
 module.exports = app;
