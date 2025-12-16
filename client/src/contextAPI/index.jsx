b20be6b9e381908932ead8316b6175e5963ddaea
Shantivaghela <shantivaghela2019@gmail.com>
2025-12-16 17:57:47 +0530

    socket fix

--- client/src/contextAPI/index.jsx
+++ client/src/contextAPI/index.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 const API = import.meta.env.VITE_API_URL;
-console.log(API);
 
 
 export const AuthContext = createContext();
