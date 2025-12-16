d4e7944f01686e6ef8d8b4ef432d0c8da4c3a0bd
Shantivaghela <shantivaghela2019@gmail.com>
2025-12-16 18:12:06 +0530

    socket change and fix

--- server/utils/socketUsers.js
+++ server/utils/socketUsers.js
@@ -8,7 +8,7 @@ const io = new Server(server, {
     cors:{
     origin:["http://localhost:5173","http://localhost:5174","https://connectyfyweb.vercel.app"],
     method:["GET,POST,PUT,DELETE,PATCH,HEAD"],
-    // Credentials:true
+    Credentials:true
     }
 });
 
