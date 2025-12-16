bda90ffb8a6453970b773841baeea8bd3783a994
Shantivaghela <shantivaghela2019@gmail.com>
2025-12-16 22:31:16 +0530

    fixed routes

--- client/src/pages/Message.jsx
+++ client/src/pages/Message.jsx
@@ -79,12 +79,12 @@ function Message() {
     }, []);
     return (
         <>
-            <section className="md:ml-96 h-full mt-22 w-full relative flex justify-center flex-col overflow-y-scroll scobar">
-                <div className="flex   w-full mb-5 p-4 rounded-xl items-center dark:bg-gray-800 bg-[#f2efe6]  z-12 md:gap-10 gap-5 mx-2 dark:text-white">
+            <section className="md:ml-96 h-full mt-22 relative flex justify-center flex-col overflow-y-scroll scobar">
+                <div className="flex   w-[90%] mb-5 p-4 rounded-xl items-center dark:bg-gray-800 bg-[#f2efe6]  z-12 md:gap-10 gap-5 mx-2 dark:text-white">
                     <button onClick={() => navigate(-1)} className='cursor-pointer '>
                         <i class="fa-solid fa-arrow-left text-2xl md:text-3xl"></i>
                     </button>
-                    <h1 className='text-2xl md:text-3xl '>Messages jll</h1>
+                    <h1 className='text-2xl md:text-3xl '>Messages</h1>
                 </div>
                 <div className="flow-root  px-3 h-full overflow-hidden overflow-y-scroll scobar ">
 
